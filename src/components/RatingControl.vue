33eb510c8e382c4148c36d3c2f8efd4ee904c83e
Piotr9 <33264584+Piotr9@users.noreply.github.com>
2021-02-07 23:56:46 +0100

    First commit
    First form added
    -input
    -dropdown menu
    -checkboxes
    - custom component

---
+++ src/components/RatingControl.vue
@@ -0,0 +1,61 @@
+<template>
+    <ul>
+        <li :class="{active: modelValue === 'poor'}"><button type="button" @click="activate('poor')">Poor</button></li>
+        <li :class="{active: modelValue === 'avarage'}"><button type="button" @click="activate('avarage')">Avarage</button></li>
+        <li :class="{active: modelValue === 'great'}"><button type="button" @click="activate('great')">Great</button></li>
+    </ul>
+</template>
+
+<script>
+export default {
+    props: ['modelValue'],
+    emits: ['update:modelValue'],
+    // data() {
+    //     return {
+    //         activeOption: this.modelValue,
+    //     };
+    // },
+    // computed: {
+    //     activeOption() {
+    //         return this.modelValue;
+    //     }
+    // },
+    methods: {
+        activate(option) {
+            this.$emit('update:modelValue', option);
+        },
+    },
+};
+</script>
+
+<style scoped>
+.active {
+    border-color: rgb(173, 20, 135);
+}
+
+.active button {
+    color: rgb(173, 20, 135);
+}
+
+ul {
+    list-style: none;
+    margin: 0.5rem 0;
+    padding: 0;
+    display: flex;
+    }
+li {
+    margin: 0 1rem;
+    border: 2px solid #ccc;
+    padding: 0.5rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+button {
+    font: inherit;
+    border: none;
+    background-color: transparent;
+    cursor: pointer;
+}
+</style>
